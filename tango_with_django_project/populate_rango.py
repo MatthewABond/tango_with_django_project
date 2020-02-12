614d74a38861d21d55b5956214e552d146f6d072
Matthew Bond <2393814b@student.gla.ac.uk>
2020-02-12 19:07:23 +0000

    Completed Chapter 6

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -6,49 +6,30 @@ django.setup()
 from rango.models import Category, Page
 
 def populate():
-	# First, we will create lists of dictionaries containing the pages
-	# we want to add into each category.
-	# Then we will create a dictionary of dictionaries for our categories.
-	# This might seem a little bit confusing, but it allows us to iterate
-	# through each data structure, and add the data to our models.
-
+	
 	python_pages = [
-		{'title': 'Official Python Tutorial',
-		'url':'http://docs.python.org/3/tutorial/'},
-		{'title':'How to Think like a Computer Scientist',
-		'url':'http://www.greenteapress.com/thinkpython/'},
-		{'title':'Learn Python in 10 Minutes',
-		'url':'http://www.korokithakis.net/tutorials/python/'} ]
+		{'title': 'Official Python Tutorial', 'url':'http://docs.python.org/3/tutorial/', 'views' : 15},
+		{'title':'How to Think like a Computer Scientist', 'url':'http://www.greenteapress.com/thinkpython/', 'views' : 3},
+		{'title':'Learn Python in 10 Minutes', 'url':'http://www.korokithakis.net/tutorials/python/', 'views' : 54} ]
 
 	django_pages = [
-		{'title':'Official Django Tutorial',
-		'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
-		{'title':'Django Rocks',
-		'url':'http://www.djangorocks.com/'},
-		{'title':'How to Tango with Django',
-		'url':'http://www.tangowithdjango.com/'} ]
+		{'title':'Official Django Tutorial', 'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/', 'views' : 32},
+		{'title':'Django Rocks', 'url':'http://www.djangorocks.com/', 'views' : 12},
+		{'title':'How to Tango with Django', 'url':'http://www.tangowithdjango.com/', 'views' : 68} ]
 
 	other_pages = [
-		{'title':'Bottle',
-		'url':'http://bottlepy.org/docs/dev/'},
-		{'title':'Flask',
-		'url':'http://flask.pocoo.org'} ]
+		{'title':'Bottle', 'url':'http://bottlepy.org/docs/dev/', 'views' : 10},
+		{'title':'Flask', 'url':'http://flask.pocoo.org', 'views' : 2} ]
 		
 	cats = {'Python': {'pages': python_pages, 'views' : 128, 'likes' : 64},
 			'Django': {'pages': django_pages, 'views' : 64, 'likes' : 32},
 			'Other Frameworks': {'pages': other_pages, 'views' : 32, 'likes' : 16} }
 			
-	# If you want to add more categories or pages,
-	# add them to the dictionaries above.
-
-	# The code below goes through the cats dictionary, then adds each category,
-	# and then adds all the associated pages for that category.
 	for cat, cat_data in cats.items():
 		c = add_cat(cat, views = cat_data['views'], likes = cat_data['likes'])
 		for p in cat_data['pages']:
-			add_page(c, p['title'], p['url'])
+			add_page(c, p['title'], p['url'], p['views'])
 
-	# Print out the categories we have added.
 	for c in Category.objects.all():
 		for p in Page.objects.filter(category=c):
 			print(f'- {c}: {p}')
@@ -67,7 +48,6 @@ def add_cat(name, views=0, likes=0):
 	c.save()
 	return c
 
-# Start execution here!
 if __name__ == '__main__':
 	print('Starting Rango population script...')
 	populate()
